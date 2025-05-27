105855a564a0099631dd7846b99b9c06d967fce7
unknown <Administrador@lci-8-7.upf.br>
2025-05-26 22:02:41 -0300

    criando MVC professores

---
+++ app/Http/Controllers/ProfessorController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfessorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+ 
+        $professores = Professor::all();
+        return view('professores.index', compact('professores'));
+    }
+
+    public function create()
+    {
+        return view('professores.create');
+    }
+
+    public function store(Request $request)
+    {
+        $professor = new Professor([
+            'nome' => $request->input('nome'),
+            'telefone' => $request->input('telefone'),
+            'email' => $request->input('email')
+        ]);
+        $professor-> save();
+        return redirect()->route('professores.index');
+    }
+
+    public function show(string $id)
+    {
+        $professor = Professor::findOrFail($id);
+        return view('professores.show', ['professor'=> $professor]);
+    }
+
+    public function edit(string $id)
+    {
+        $professor = Professor::findOrFail($id);
+        return view('professores.edit', compact('professor'));
+    }
+
+    public function update(Request $request, string $id)
+    {
+        
+        $professor->update($professores)
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ app/Models/Professor.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Professor extends Model
+{
+    protected $table ='professores';
+    protected $fillable = ['nome','telefone','email']
+}
